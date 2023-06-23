14be689a6fa3c715e6763be457f429eb8c0a3735
JUNYONG <wnsdyd1489@gmail.com>
2023-06-23 10:22:27 +0900

    create_AICI

---
+++ Front-end/construction/static/construction/style.js
@@ -0,0 +1,118 @@
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 4 // 지도의 확대 레벨
+    };  
+
+// 지도를 생성합니다    
+var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+// 주소-좌표 변환 객체를 생성합니다
+var geocoder = new kakao.maps.services.Geocoder();
+
+
+document.getElementById('click').onclick = function(){
+    // 주소로 좌표를 검색합니다
+
+    let address = document.getElementById('input').value
+    geocoder.addressSearch(address, function(result, status) {
+
+        // 정상적으로 검색이 완료됐으면 
+         if (status === kakao.maps.services.Status.OK) {
+    
+            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+    
+            // 결과값으로 받은 위치를 마커로 표시합니다
+            var marker = new kakao.maps.Marker({
+                map: map,
+                position: coords
+            });
+    
+            // 인포윈도우로 장소에 대한 설명을 표시합니다
+            var infowindow = new kakao.maps.InfoWindow({
+                content: '<div style="width:150px;text-align:center;padding:6px 0;">KT 전남 본부</div>'
+            });
+            infowindow.open(map, marker);
+    
+            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+            map.setCenter(coords);
+        }
+    });    
+}
+
+
+
+
+let date = new Date();
+
+const renderCalender = () => {
+    const viewYear = date.getFullYear();
+    const viewMonth = date.getMonth();
+
+    document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth +1}월`;
+
+    const prevLast = new Date(viewYear, viewMonth, 0);
+    const thisLast = new Date(viewYear, viewMonth + 1,0);
+
+    const PLDate = prevLast.getDate();
+    const PLDay = prevLast.getDay();
+
+    const TLDate = thisLast.getDate();
+    const TLDay = thisLast.getDay();
+
+    const prevDates = [];
+    const thisDates = [...Array(TLDate + 1).keys()].slice(1);
+    const nextDates = [];
+
+    if (PLDay !==6) {
+        for (let i = 0; i< PLDay + 1; i++) {
+            prevDates.unshift(PLDate - i);
+        }
+    }
+
+    for (let i = 1; i < 7 - TLDay; i++) {
+        nextDates.push(i);
+    }
+
+    const dates = prevDates.concat(thisDates, nextDates);
+    const firstDateIndex = dates.indexOf(1);
+    const lastDateIndex = dates.lastIndexOf(TLDate);
+
+    dates.forEach((date, i) => {
+        const condition = i >= firstDateIndex && i < lastDateIndex + 1
+                          ? 'this'
+                          : 'other';
+        dates[i] = `<div class="date"><span class=${condition}>${date}</span></div>`;
+    });
+
+    document.querySelector('.dates').innerHTML = dates.join('');
+
+    const today = new Date();
+    if (viewMonth === today.getMonth() && viewYear === today.getFullYear()) {
+        for (let date of document.querySelectorAll('.this')) {
+            if (+date.innerText === today.getDate()) {
+                date.classList.add('today');
+                break;
+            }
+        }
+    }
+};
+
+renderCalender();
+
+const prevMonth = () =>{
+    date.setDate(1);
+    date.setMonth(date.getMonth()-1);
+    renderCalender();
+};
+
+const nextMonth = () => {
+    date.setDate(1);
+    date.setMonth(date.getMonth()+1);
+    renderCalender();
+};
+
+const goToday = () => {
+    date = new Date();
+    renderCalender();
+};
---
+++ Front-end/mysite/urls.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'index.html')
+
+urlpatterns = [
+    path('',index),
+    path('admin/', admin.site.urls),
+    path('privacy_policy/', include("privacy_policy.urls")), # 개인정보
+    path('terms_of_service/', include("terms_of_service.urls")), # 이용약관
+    path('join/',include('join.urls')), # 회원가입
+    path('construction/',include('construction.urls')), # 시외공사
+    path('home/',include('home.urls')), # 홈
+    path('notice/',include('notice.urls')), # 공지사항
+
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
