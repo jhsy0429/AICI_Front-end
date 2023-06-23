9813f1dc81ea51ea0d349790af1418203eb761ad
JUNYONG <wnsdyd1489@gmail.com>
2023-06-23 13:44:33 +0900

    width

--- Front-end/construction/static/construction/style.js
+++ Front-end/construction/static/construction/style.js
@@ -1,4 +1,4 @@
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+var mapContainer = document.getElementById('kakaomap'), // 지도를 표시할 div 
     mapOption = {
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
         level: 4 // 지도의 확대 레벨
@@ -39,80 +39,3 @@ document.getElementById('click').onclick = function(){
         }
     });    
 }
-
-
-
-
-let date = new Date();
-
-const renderCalender = () => {
-    const viewYear = date.getFullYear();
-    const viewMonth = date.getMonth();
-
-    document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth +1}월`;
-
-    const prevLast = new Date(viewYear, viewMonth, 0);
-    const thisLast = new Date(viewYear, viewMonth + 1,0);
-
-    const PLDate = prevLast.getDate();
-    const PLDay = prevLast.getDay();
-
-    const TLDate = thisLast.getDate();
-    const TLDay = thisLast.getDay();
-
-    const prevDates = [];
-    const thisDates = [...Array(TLDate + 1).keys()].slice(1);
-    const nextDates = [];
-
-    if (PLDay !==6) {
-        for (let i = 0; i< PLDay + 1; i++) {
-            prevDates.unshift(PLDate - i);
-        }
-    }
-
-    for (let i = 1; i < 7 - TLDay; i++) {
-        nextDates.push(i);
-    }
-
-    const dates = prevDates.concat(thisDates, nextDates);
-    const firstDateIndex = dates.indexOf(1);
-    const lastDateIndex = dates.lastIndexOf(TLDate);
-
-    dates.forEach((date, i) => {
-        const condition = i >= firstDateIndex && i < lastDateIndex + 1
-                          ? 'this'
-                          : 'other';
-        dates[i] = `<div class="date"><span class=${condition}>${date}</span></div>`;
-    });
-
-    document.querySelector('.dates').innerHTML = dates.join('');
-
-    const today = new Date();
-    if (viewMonth === today.getMonth() && viewYear === today.getFullYear()) {
-        for (let date of document.querySelectorAll('.this')) {
-            if (+date.innerText === today.getDate()) {
-                date.classList.add('today');
-                break;
-            }
-        }
-    }
-};
-
-renderCalender();
-
-const prevMonth = () =>{
-    date.setDate(1);
-    date.setMonth(date.getMonth()-1);
-    renderCalender();
-};
-
-const nextMonth = () => {
-    date.setDate(1);
-    date.setMonth(date.getMonth()+1);
-    renderCalender();
-};
-
-const goToday = () => {
-    date = new Date();
-    renderCalender();
-};
